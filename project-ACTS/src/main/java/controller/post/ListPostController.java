2a9edc50e21a3699ccc654ee25f7b560d1445ad4
Kyung Won Kim <ddwu20190942@gmail.com>
2024-08-26 20:33:58 +0900

    fix: Main 화면 UI 개선
    - ListPostController 반환 url 수정

--- project-ACTS/src/main/java/controller/post/ListPostController.java
+++ project-ACTS/src/main/java/controller/post/ListPostController.java
@@ -21,6 +21,6 @@ public class ListPostController implements Controller {
 		List<Post> postList = manager.findPostList();
 		request.setAttribute("postList", postList);
 
-		return "/comm/mainTest.jsp";
+		return "/comm/main.jsp";
 	}
 }

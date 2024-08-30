ccd479a5331c689af38e979c60929800d26ee3a5
김경원 <ddwu20190942@gmail.com>
2024-08-30 19:44:33 +0900

    fix: UploadPostFormController 내 게시글 수정과 생성의 구분을 둔 코드 구현

--- project-ACTS/src/main/java/controller/post/UploadPostFormController.java
+++ project-ACTS/src/main/java/controller/post/UploadPostFormController.java
@@ -13,15 +13,19 @@ public class UploadPostFormController implements Controller {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		PostManager postManager = PostManager.getInstance();
-		
-		Post post = postManager.findPostByPostId(Integer.parseInt(request.getParameter("postId")));
-		
 		if (!UserSessionUtils.hasLogined(request.getSession()))
 			return "redirect:/user/login/form";
-
+		
+		if ( request.getParameter("postId") != null ) {
+			PostManager postManager = PostManager.getInstance();
+			Post post = postManager.findPostByPostId(Integer.parseInt(request.getParameter("postId")));
+			request.setAttribute("post", post);
+		} else {
+			request.setAttribute("post", null);
+		}
+		
 		request.setAttribute("loginId", UserSessionUtils.getLoginUserId(request.getSession()));
-		request.setAttribute("post", post);
+		
 		
 		return "/post/postForm.jsp";
 	}

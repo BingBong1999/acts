d3847c9d514920fc586bc7ff239a03400f2aedb8
김경원 <ddwu20190942@gmail.com>
2024-09-02 14:27:45 +0900

    feat: [채팅] 웹 소켓 클래스 구현

---
+++ project-ACTS/src/main/java/controller/chat/ChatWebSocket.java
@@ -0,0 +1,63 @@
+package controller.chat;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import com.google.gson.Gson;
+
+import model.Message;
+import model.service.ChatManager;
+
+@ServerEndpoint("/chatSocket")
+public class ChatWebSocket {
+
+    private static Set<Session> clients = Collections.synchronizedSet(new HashSet<>());
+    private ChatManager chatManager = ChatManager.getInstance();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        clients.add(session);
+        System.out.println("새로운 클라이언트 연결: " + session.getId());
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        clients.remove(session);
+        System.out.println("클라이언트 연결 종료: " + session.getId());
+    }
+
+    @OnMessage
+    public void onMessage(String messageJson, Session session) {
+        System.out.println("메시지 수신: " + messageJson);
+        Message message = new Gson().fromJson(messageJson, Message.class);
+
+        try {
+            chatManager.createMessage(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        broadcast(messageJson);
+    }
+
+    private void broadcast(String message) {
+        synchronized (clients) {
+            for (Session client : clients) {
+                try {
+                    client.getBasicRemote().sendText(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}

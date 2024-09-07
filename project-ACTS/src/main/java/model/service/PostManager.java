b5993b46bc4357fe4ae13e75abe0a3d356336c80
김경원 <ddwu20190942@gmail.com>
2024-09-07 18:23:23 +0900

    feat: [사용자 맞춤형 게시글 추천] 게시글을 JSONArray로 변환하는 메서드 구현
    - 전체 게시글을 JSONArray로 변환하는 메서드 추가
    - 특정 게시글을 JSONObject로 변환하는 메서드 추가
    - 게시글 내 본문 내 명사만 추출 (한국어 형태소 분석기 komoran 사용)
    - 게시글 id를 기반으로 게시글을 찾는 메서드 수정 ( CategoryName 설정 )

--- project-ACTS/src/main/java/model/service/PostManager.java
+++ project-ACTS/src/main/java/model/service/PostManager.java
@@ -1,11 +1,19 @@
 package model.service;
 
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import kr.co.shineware.nlp.komoran.model.KomoranResult;
+
 import java.sql.SQLException;
 
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import exception.PostNotFoundException;
 import exception.UserNotFoundException;
+
 import model.Category;
 import model.Post;
 import model.dao.PostDAO;
@@ -39,11 +47,12 @@ public class PostManager {
 	public Post findPostByPostId(int id) throws SQLException, PostNotFoundException {
 
 		Post post = postDAO.findPostByPostId(id);
-		
+
 		if (post == null) {
 			throw new PostNotFoundException(id + "는 존재하지 않는  포스트입니다.");
 		}
-
+		
+		post.setCategoryName(Category.getNameById(post.getCategoryId()));
 		post.setImageUrl(imageManager.findImageUrlsByPostId(post.getId()));
 
 		return post;
@@ -63,7 +72,6 @@ public class PostManager {
 		return postDAO.findPostsByCategory(category);
 	}
 
-
 	public void increaseViewCount(Post post) throws SQLException {
 		postDAO.increaseViewCount(post);
 	}
@@ -71,9 +79,54 @@ public class PostManager {
 	public List<Post> findPostsByAuthorId(String authorId) throws SQLException {
 		return postDAO.findPostsByAuthorId(authorId);
 	}
-	
+
 	public List<Post> findPostsByKeywordOfTitle(String keyword) throws SQLException {
 		return postDAO.findPostsByKeywordOfTitle(keyword);
+	}
+
+	public JSONArray convertAllPostToJsonArray() throws SQLException, UserNotFoundException {
+
+		List<Post> posts = findAllPosts();
+
+		JSONArray rslt = new JSONArray();
+
+		for (Post p : posts) {
+			rslt.put(new JSONObject().put("id", p.getId())
+									.put("title", p.getTitle())
+									.put("keywords", extractKeywordsFromBody(p.getBody()))
+									.put("category", p.getCategoryName()));
+		}
+
+		return rslt;
+	}
+
+	public JSONObject convertPostToJsonArray(Post post) throws SQLException, UserNotFoundException {
+
+		JSONObject rslt = new JSONObject();
+
+		rslt.put("id", post.getId())
+			.put("title", post.getTitle())
+			.put("keywords", new JSONArray(extractKeywordsFromBody(post.getBody())))
+			.put("category", post.getCategoryName());
+
+		return rslt;
+	}
+
+	public JSONArray extractKeywordsFromBody(String body) {
+
+		body = body.replaceAll("<br>", "");
+		
+		Komoran komoran = new Komoran(DEFAULT_MODEL.FULL);
+		KomoranResult analyzeResultList = komoran.analyze(body);
+		List<String> nouns = analyzeResultList.getNouns();
+		
+		JSONArray rslt = new JSONArray();
+		
+		for(String noun : nouns) {
+			rslt.put(noun);
+		}
+
+		return rslt;
 	}
 
 }
